fb60f8a400062f489efc44397dd87be6e51565ca
is-zhangzhangzhang <is_zhangjiayi@163.com>
2019-01-07 23:04:13 +0800

    入口页添加轮播图

--- code/src/components/Hello.vue
+++ code/src/components/Hello.vue
@@ -16,16 +16,21 @@
       </el-header>
       <el-main>
         <el-row gutter="30" type="flex" align="middle" class="main-nav">
-            <el-col :span="3"><img src="../assets/images/index/login-main.png" alt="乐游社区"></el-col>
-            <el-col :span="9" :offset="12">
-              <el-button type="warning" size="large">登录</el-button>
-              <el-button type="danger" plain size="large">注册</el-button>
+            <el-col :span="4">
+              <img src="../assets/images/index/login-main.png" alt="乐游社区">
+            </el-col>
+            <el-col :span="6">
+              <div><strong>爱分享，乐旅游</strong></div>
+            </el-col>
+            <el-col :span="9" :offset="5">
+              <el-button type="warning" @click="login_modal">登录</el-button>
+              <el-button type="danger" plain>注册</el-button>
             </el-col>
         </el-row>
         <el-row>
           <div class="block">
           <span class="demonstration">
-            <el-carousel trigger="click" height="calc( 100vh - 140px )">
+            <el-carousel trigger="click" height="calc( 100vh - 160px )">
               <el-carousel-item v-for="item in 4" :key="item">
                 <h3>{{ item }}</h3>
               </el-carousel-item>
@@ -40,10 +45,37 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  methods: {
+    login_modal () {
+      const h = this.$createElement
+      this.$msgbox({
+        title: '登录',
+        message: h('p', null, [
+          h('span', null, '内容可以是 '),
+          h('i', { style: 'color: teal' }, 'VNode')
+        ]),
+        showCancelButton: true,
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        beforeClose: (action, instance, done) => {
+          if (action === 'confirm') {
+            instance.confirmButtonLoading = true
+            instance.confirmButtonText = '登录中...'
+            setTimeout(() => {
+              done()
+              setTimeout(() => {
+                instance.confirmButtonLoading = false
+              }, 300)
+            }, 3000)
+          } else {
+            done()
+          }
+        }
+      }).then(action => {
+        this.$message({
+          type: 'info',
+          message: 'action: ' + action})
+      })
     }
   }
 }
@@ -94,9 +126,44 @@ export default {
     height: 70px;
     margin: 15px 0;
   }
-  .el-carousel__item:nth-child(n) {
+  .main-nav > .el-col:nth-child(2) > div{
+    margin-left: 15px;
+    padding-left: 15px;
+    font-size: 18px;
+    text-align: left;
+    border-left: 2px solid rgb(249,162,50);
+    color: rgb(249,162,50);
+  }
+  .el-carousel__item h3 {
+    color: #9c9898;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 100px;
+    margin: 0;
+  }
+  .el-carousel__item{
     background-size: cover;
+    background-repeat: no-repeat;
+  }
+  .el-carousel__item:nth-child(5) {
     background-image: url("../assets/images/index/carousel1.jpg");
+  }
+  .el-carousel__item:nth-child(6) {
+    background-image: url("../assets/images/index/carousel2.jpg");
+  }
+  .el-carousel__item:nth-child(3) {
+    background-image: url("../assets/images/index/carousel5.jpg");
+  }
+  .el-carousel__item:nth-child(4) {
+    background-image: url("../assets/images/index/carousel4.jpg");
+  }
+  /*@media screen and (max-width: 1000px){*/
+    /*.el-carousel__item:nth-child(0) {*/
+      /*background-image: url("../assets/images/index/carousel1-small.jpg");*/
+    /*}*/
+  /*}*/
+  .main-nav > .el-col > button{
+    font-size: 1.2rem;
   }
   /*.el-carousel__item:nth-child(2n) {*/
     /*background-color: #99a9bf;*/
